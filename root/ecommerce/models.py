d2f1fe08000779b9bcdb1a7560f2a7477d4fc0b0
Muksam <Muksam@example.com>
2024-09-24 19:50:01 +0545

    Project Setup and Design the Database for Ecommerce Website

---
+++ root/ecommerce/models.py
@@ -0,0 +1,107 @@
+from django.db import models
+
+from root.utils import BaseModel
+from users.models import User
+# Create your models here.
+
+class Category(BaseModel):
+    name = models.CharField(max_length=100, unique = True)
+    slug = models.SlugField(unique = True)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+
+class Product(BaseModel):
+    name = models.CharField(max_length=100, unique = True)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default = 0)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE, related_name = "product_category")
+    image = models.ImageField(upload_to = "product/images", null = False, blank = False)
+    updated_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+
+class Address(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "user_address")
+    street_address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=100)
+    is_default_shipping = models.BooleanField(default=False)
+    is_default_billing = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.street_address}, {self.city}, {self.state}, {self.country}"
+    
+
+class Order(BaseModel):
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "user_orders")
+    order_date = models.DateField()
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    STATUS = (
+        ("Pending","Pending"),
+        ("Completed", "Completed"),
+        ("Cancelled", "Cancelled"),
+        ("None", "None")
+    )
+    status = models.CharField(max_length=20, choices = STATUS, default = "None")
+    shipping_address = models.ForeignKey(Address, related_name='shipping_orders', on_delete=models.SET_NULL, null=True)
+    billing_address = models.ForeignKey(Address, related_name='billing_orders', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}"
+    
+
+class OrderItem(BaseModel):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name = "order_product_items")
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name} in Order #{self.order.id}"
+    
+
+
+class Cart(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name = "user_cart_items")
+
+    def __str__(self):
+        return f"Cart for {self.user.username}"
+    
+
+
+class CartItem(BaseModel):
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name = "product_cart_items")
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name} in Cart"
+
+
+class Review(BaseModel):
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "users_review")
+    product = models.ForeignKey(Product, on_delete = models.CASCADE, related_name = "product_review")
+    rating = models.PositiveIntegerField(choices = [(i, str(i)) for i in range(1, 6)])
+    comment = models.TextField(null = False, blank = False)
+
+    def __str__(self):
+        return f"Review for {self.product.name} by {self.user.username}"
+    
+
+class Discount(BaseModel):
+    code = models.CharField(max_length=20, unique=True)
+    percentage = models.DecimalField(max_digits=5, decimal_places=2)  # e.g., 10.00 for 10%
+    active = models.BooleanField(default=True)
+    start_date = models.DateTimeField(null=True, blank=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.code}"
---
+++ root/users/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+from root.utils import BaseModel
+from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
+# Create your models here.
+
+USER_CHOICES = (
+      ("Customer", "Customer"),
+      ("None","None")
+)
+
+class User(AbstractUser, BaseModel):
+    username = models.CharField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=20, unique=True, null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(max_length=255, unique=True, null=False, blank=False)
+    email_verified = models.BooleanField(default=False)
+    user_type = models.CharField(max_length=255, choices=USER_CHOICES, default="None")
+    user_image = models.ImageField(upload_to="user/images/", null=True, blank=True)
+    full_name = models.CharField(max_length=255, null=True)
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return f"{self.email}"
